32fc20332b9e66e21bf4e4884b6557c248a99844
HeikoR <heikor93@outlook.de>
2025-06-11 06:42:51 +0000

    added reset/filter btn

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const input = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const list = document.getElementById("todo-list");
 const inputrow = document.getElementById ("input_row");
+const inputfield = document.getElementById ("todo-input");
 const delBtn = document.createElement ("button");
 const sortBtn = document.createElement ("button");
+const rstBtn = document.createElement ("button");
+const fltBtn = document.createElement ("button");
 
 addBtn.addEventListener("click", () => {                      //add task event listener
   const text = input.value.trim();                            //check valid input
@@ -23,12 +26,20 @@ addBtn.addEventListener("click", () => {                      //add task event l
   li.appendChild(checkbox);
   input.value = "";                                           //clear previous input
 
-  // add delete button
+  // add delete, reset, filter button
 
   if (list.children.length > 0 && list.children.length <= 1) { 
     delBtn.id = "del-btn";
     delBtn.textContent = "Delete";
     inputrow.appendChild(delBtn);
+
+    rstBtn.id = "rst-btn";
+    rstBtn.textContent = "Reset";
+    inputrow.insertBefore(rstBtn, inputfield);
+
+    fltBtn.id = "flt-btn";
+    fltBtn.textContent = "Filter";
+    inputrow.insertBefore(fltBtn, inputfield);
    }
 
    //add sort button
@@ -54,8 +65,10 @@ delBtn.addEventListener("click", () => {
   //if no list item / delete sort/delete button 
 
   if (list.children.length === 0) {
-    document.getElementById("del-btn").remove();
+    delBtn.remove();
     if (sortBtn) sortBtn.remove();
+    if (rstBtn) rstBtn.remove();
+    if (fltBtn) fltBtn.remove();
   }
 });
 
@@ -73,3 +86,30 @@ delBtn.addEventListener("click", () => {
     list.innerHTML = "";
     items.forEach(item => list.appendChild(item));
 });
+
+  // reset button function
+
+  rstBtn.addEventListener("click", () => {
+    const items = list.querySelectorAll("li");
+    items.forEach (item => item.remove());
+    rstBtn.remove();
+    fltBtn.remove();
+    delBtn.remove();
+    sortBtn.remove();
+  });
+
+   // filter button function
+
+  let filtered = false;
+
+  fltBtn.addEventListener("click", () => {
+    const tasks = list.querySelectorAll("li");
+    tasks.forEach(task => {
+    const checkbox = task.querySelector("input[type='checkbox']");
+    if (checkbox && checkbox.checked) {
+    task.style.display = filtered ? "list-item" : "none" ;
+    }
+    });
+    fltBtn.textContent = filtered ? "Filter" : "Show All"
+    filtered = !filtered;
+  });
